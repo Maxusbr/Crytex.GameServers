debcdfd37a4e302fb24bd4f5e732006ba32793d9
Maxusbr <max_73@inbox.ru>
2016-02-12 17:52:03 +0300

    CS

---
+++ Interface/ISshCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.GameServers.Interface
+{
+    public interface ISshCommand
+    {
+        void Go(string userId, string id);
+        void On(string userId, string id);
+        void Off(string userId, string id);
+    }
+}
